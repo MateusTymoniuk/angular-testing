45a4c8c84e8c2f672252fb58fa954a725caf7142
Mateus Tymoniuk <tymoniuk.m@gmail.com>
2019-11-20 21:55:04 -0300

    Creating test for request that return error and for request with parameters

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,8 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CoursesService } from './courses.service';
-import { COURSES } from '../../../../server/db-data';
+import { COURSES, findLessonsForCourse } from '../../../../server/db-data';
 import { Course } from '../model/course';
+import { HttpErrorResponse } from '@angular/common/http';
 
 describe('CoursesService', () => {
 
@@ -81,6 +82,56 @@ describe('CoursesService', () => {
         ...changes
       }
     );
+  });
+
+  it('Should return an error if save course fails', () => {
+
+    const changes: Partial<Course> = {
+      titles:
+      {
+        description: 'TestingCourse'
+      }
+    };
+
+    coursesService.saveCourse(12, changes)
+      .subscribe(
+        () => fail('The save course operation should have failed'),
+        (error: HttpErrorResponse) => {
+          expect(error.status).toBe(500);
+          expect(error.statusText).toBe('Internal Server Error');
+        });
+
+    const req = httpTestingController.expectOne('/api/courses/12');
+
+    expect(req.request.method).toEqual('PUT');
+
+    req.flush(null, { status: 500, statusText: 'Internal Server Error' });
+  });
+
+  it('Should find a list of lessons', () => {
+    coursesService.findLessons(12).subscribe(
+      lessons => {
+        expect(lessons).toBeTruthy();
+        expect(lessons.length).toBe(3);
+      }
+    );
+
+    const req = httpTestingController.expectOne(
+      request => request.url === '/api/lessons'
+    );
+
+    expect(req.request.method).toEqual('GET');
+
+    expect(req.request.params.get('courseId')).toEqual('12');
+    expect(req.request.params.get('filter')).toEqual('');
+    expect(req.request.params.get('sortOrder')).toEqual('asc');
+    expect(req.request.params.get('pageNumber')).toEqual('0');
+    expect(req.request.params.get('pageSize')).toEqual('3');
+
+    req.flush({
+      payload: findLessonsForCourse(12).slice(0, 3)
+    });
+
   });
 
 });
